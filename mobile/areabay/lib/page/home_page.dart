14a88f183e5a391415ffbb10f032e0c53e9d7aa4
Anthony COUHIER <anthony.couhier@epitech.eu>
2022-02-10 19:09:11 +0100

    feat: home page, show bay

--- mobile/areabay/lib/page/home_page.dart
+++ mobile/areabay/lib/page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home/list.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,16 +9,53 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    ShowAllAction(),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(),
         home: Scaffold(
-          appBar: AppBar(title: const Text("Action list")),
-          body: const Text('aze'),
-          // showAction(
-          //   reactionIcon: "reaction",
-          //   actionIcon: "action",
-        ));
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.apps),
+            label: 'List',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_to_photos_sharp),
+            label: 'Create',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        backgroundColor: const Color.fromRGBO(34, 79, 129, 1),
+        selectedItemColor: const Color.fromARGB(255, 197, 197, 197),
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+      body: SafeArea(child: _widgetOptions.elementAt(_selectedIndex)),
+    ));
   }
 }
