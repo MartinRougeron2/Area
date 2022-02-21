8d26b0d331bb7c014406387fade06be364cb4236
martin <martin.rougeron@epitech.eu>
2022-02-21 14:09:16 +0100

    feat admin token

--- backend/authentification_server/apollo_client.ts
+++ backend/authentification_server/apollo_client.ts
@@ -2,7 +2,10 @@ import ApolloClient from "apollo-boost";
 import 'cross-fetch/polyfill';
 
 const client = new ApolloClient({
-    uri: "http://localhost:5000/graphql"
+    uri: "http://localhost:5000/graphql",
+    headers: {
+        "x-token": process.env.ADMIN_JWT
+    }
 });
 
 export {client};
