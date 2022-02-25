160790bbd512ecd527514771d8493a1fa2a203bc
Anthony COUHIER <anthony.couhier@epitech.eu>
2022-02-25 15:13:41 +0100

    feat: horizontal action to remove item (in show list)

--- mobile/areabay/lib/page/home_page.dart
+++ mobile/areabay/lib/page/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'home/list.dart';
+import 'home/list/list.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
