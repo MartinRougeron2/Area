3bd64e0ee42fa9359323365a83e7959f8d7697bb
Alexandre Bidault <alexandre.bidault@epitech.eu>
2022-03-03 18:16:27 +0100

    start github service

--- backend/authentification_server/server.ts
+++ backend/authentification_server/server.ts
@@ -24,6 +24,7 @@ require("./gcalendar_oauth")(auth_server)
 require("./gmail_oauth")(auth_server)
 require("./discord_oauth")(auth_server)
 require("./discord_bot_oauth")(auth_server)
+require("./github_oauth")(auth_server)
 
 auth_server.get('/auth/finish', ((__req: express.Request, res: any) => {
     res.send('<h1> Finished ! </h1><h4>You can close it</h4>')
