480b022d139ebd8f82ec7cd8024edb7e441d6cf6
Alexandre Bidault <alexandre.bidault@epitech.eu>
2022-03-04 11:48:37 +0100

    add github service

--- backend/microservices/server.ts
+++ backend/microservices/server.ts
@@ -24,6 +24,7 @@ require('./graphql')(app);
 // require('./services/gcalendar_in')
 require('./services/oauth2')
 require('./services/discord_in')
+require('./services/github_in')
 //require('./services/gmail_in')
 
 //task.start()
