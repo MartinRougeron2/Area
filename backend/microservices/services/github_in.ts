b02e49006a8144aee0345797a7de92a16d2e3d34
Alexandre Bidault <alexandre.bidault@epitech.eu>
2022-03-04 11:48:37 +0100

    add github service

--- backend/microservices/services/github_in.ts
+++ backend/microservices/services/github_in.ts
@@ -15,11 +15,11 @@ async function getNumberRepo(token_access: string) : Promise<string> {
         }
     })
     const repo = await response.json()
-   return repo.length.toString()
+   return repo.length
 }
 
 var task = cron.schedule('15 * * * * *', () => {
-    ServiceModel.findOne({name: "Discord"}).populate('actions').then((res) => {
+    ServiceModel.findOne({name: "Github"}).populate('actions').then((res) => {
         if (!res?.actions) return // null verif
         UniqueActionModel.find({action: {"$in": res.actions}}).populate('action').then(async (res_unique_actions) => {
                 if (!res_unique_actions) return // null verif
@@ -30,12 +30,12 @@ var task = cron.schedule('15 * * * * *', () => {
                     if (unique_actions.action == null) continue;
                     console.log(unique_actions)
                     const obj = JSON.parse(unique_actions.parameters)
-                    if (!unique_actions.old_values) {
-                        unique_actions.old_values = JSON.stringify({number: "0"})
-                    }
                     // @ts-ignore
                     if (unique_actions.action.name == "fetchNewRepositories") {
-                        const params = {number: await getNumberRepo(obj.access_token)}
+                        if (!unique_actions.old_values) {
+                            unique_actions.old_values = JSON.stringify({number: "0"})
+                        }
+                        const params = {number: await getNumberRepo(obj.access_token).toString()}
                         if (unique_actions.old_values != JSON.stringify(params)) {
                             if (parseInt(params.number) > parseInt(JSON.parse(unique_actions.old_values).number)) {
                                 await trigger_effects(unique_actions, `New repository github have been created`)
