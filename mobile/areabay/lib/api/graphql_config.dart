c0884dcd5f19e63ba2d5dc7034b6705b734c845d
Anthony COUHIER <anthony.couhier@epitech.eu>
2022-03-06 21:33:38 +0100

    fix: port

--- mobile/areabay/lib/api/graphql_config.dart
+++ mobile/areabay/lib/api/graphql_config.dart
@@ -3,16 +3,16 @@ import 'package:http/http.dart' as http;
 
 class GraphQLConfig {
 
-  static const String urlGraphQL = 'http://10.0.2.2:5000/graphql';
+  static const String urlGraphQL = 'http://10.0.2.2:8080/graphql';
 
   static Map<String, String> header = {
     "Content-Type": "application/json",
-    "x-token": ""
   };
 
   static Future<Map> postRequest(Map convertBody) async {
     String body = json.encode(convertBody).toString();
 
+    print(body);
     return json.decode((await http.post(
         Uri.parse(GraphQLConfig.urlGraphQL),
         headers: GraphQLConfig.header, body: body
