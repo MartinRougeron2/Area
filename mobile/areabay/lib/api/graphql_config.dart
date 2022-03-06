59a36b49a12b5741c3fe614637a2f7179bc253dc
Anthony COUHIER <anthony.couhier@epitech.eu>
2022-03-06 14:37:55 +0100

    fix: get token

--- mobile/areabay/lib/api/graphql_config.dart
+++ mobile/areabay/lib/api/graphql_config.dart
@@ -2,14 +2,12 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class GraphQLConfig {
-  static const String token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMTM5MTA2NDYwNGNmN2JiMzcxYWQ2MCIsImlhdCI6MTY0NjMwNzIzOCwiZXhwIjoxNjQ2OTEyMDM4fQ.7KfnUG5utlvbOlkcEiH6vlrTH-H-U1tHNRnV-nd-cGc";
 
   static const String urlGraphQL = 'http://10.0.2.2:5000/graphql';
 
   static const Map<String, String> header = {
     "Content-Type": "application/json",
-    "x-token": token
+    "x-token": ""
   };
 
   static Future<Map> postRequest(Map convertBody) async {
