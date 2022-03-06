79891250dea01423da6b23b7254864ec0e1d20eb
Anthony COUHIER <anthony.couhier@epitech.eu>
2022-03-06 12:10:32 +0100

    feat: Select action and reaction

--- mobile/areabay/lib/api/query.dart
+++ mobile/areabay/lib/api/query.dart
@@ -7,6 +7,21 @@ query Login(\$password: String!, \$email: String!) {
 }
 """;
 
+String getAllService = """
+query get_services {
+  GetAllServices {
+    id
+    name
+    actions {
+      id
+      name
+      options
+      type
+    }
+  }
+}
+""";
+
 String userGoogle = """
 query Login(\$token: String!, \$email: String!) {
   LoginUserWithGoogle(data: {email: \$email, token: \$token}) {
--- mobile/areabay/lib/page/home_page.dart
+++ mobile/areabay/lib/page/home_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'home/create/create.dart';
 import 'home/create/link_account.dart';
 import 'home/list/list.dart';
-
+import 'package:areabay/globals.dart' as global;
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -14,7 +14,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
 
   static const List<Widget> _widgetOptions = <Widget>[
     ShowAllAction(),
@@ -33,32 +33,36 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        routes: <String, WidgetBuilder>{
-          "/homePage/create/linkAccount": (BuildContext context) => const LinkAccount(),
-        },
-        home: Scaffold(
-          bottomNavigationBar: BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.apps),
-                label: 'List',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.add_to_photos_sharp),
-                label: 'Create',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.settings),
-                label: 'Settings',
-              ),
-            ],
-            backgroundColor: const Color.fromRGBO(34, 79, 129, 1),
-            selectedItemColor: const Color.fromARGB(255, 197, 197, 197),
-            currentIndex: _selectedIndex,
-            onTap: _onItemTapped,
-          ),
-          body: SafeArea(child: _widgetOptions.elementAt(_selectedIndex)),
-        ));
+    return GraphQLProvider(
+      client: global.client,
+      child: MaterialApp(
+          routes: <String, WidgetBuilder>{
+            "/homePage/create/linkAccount": (
+                BuildContext context) => const LinkAccount(),
+          },
+          home: Scaffold(
+            bottomNavigationBar: BottomNavigationBar(
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.apps),
+                  label: 'List',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.add_to_photos_sharp),
+                  label: 'Create',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.settings),
+                  label: 'Settings',
+                ),
+              ],
+              backgroundColor: const Color.fromRGBO(34, 79, 129, 1),
+              selectedItemColor: const Color.fromARGB(255, 197, 197, 197),
+              currentIndex: _selectedIndex,
+              onTap: _onItemTapped,
+            ),
+            body: SafeArea(child: _widgetOptions.elementAt(_selectedIndex)),
+          )),
+    );
   }
 }
